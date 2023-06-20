9837def78c9a25fe1721fa6999271bb14cc4c92b
natihash <natnaelfanose56@gmail.com>
2023-06-20 10:49:56 +0800

    modified datasets

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -7,6 +7,7 @@ from skimage.draw import polygon
 from skimage.draw import polygon_perimeter
 from skimage.filters import threshold_otsu
 from skimage.draw import disk
+import ntpath
 
 def xml_to_semantic(xml):
 	tree = ET.parse(xml)
@@ -33,6 +34,40 @@ def xml_to_semantic(xml):
 		sema[rr, cc] = 255
 	return sema
 
-yy = xml_to_semantic("Monuseg_dataset/Annotations/TCGA-18-5592-01Z-00-DX1.xml")
-cv2.imwrite("jhfj.png", 255*yy)
-# print(yy)
+def xml_to_marker(xml):
+	tree = ET.parse(xml)
+	root = tree.getroot()
+	mark = np.zeros((1000, 1000))
+	for i in range(1, len(root[0][1])):
+		temp = np.zeros((1000, 1000))
+		x_pts = []
+		y_pts = []
+		pts = []
+		for j in range(len(root[0][1][i][1])):
+			x = int(float(root[0][1][i][1][j].attrib['Y']))
+			if x >= 1000:
+				x = 999
+			y = int(float(root[0][1][i][1][j].attrib['X']))
+			if y >= 1000:
+				y = 999
+			x_pts.append(x)
+			y_pts.append(y)
+			pts.append([y, x])
+		x_pts = np.array(x_pts)
+		y_pts = np.array(y_pts)
+		pts = np.array(pts)
+		rr, cc = polygon(x_pts, y_pts)
+		temp[rr, cc] = 255
+		temp = temp.astype('uint8')
+		temp = cv2.distanceTransform(temp, distanceType=cv2.DIST_L2, maskSize=3, dstType=cv2.CV_8U)
+		temp = 1.0*(temp > np.amax(temp)/2)
+		mark[temp>0] = 255
+	return mark
+
+xmls = glob.glob("Monuseg_dataset/Annotations/*")
+for i, name in enumerate(xmls):
+	sema = xml_to_semantic(name)
+	mark = xml_to_marker(name)
+	cv2.imwrite("modified dataset/semas/"+ntpath.basename(name)[:-4]+".png", sema)
+	cv2.imwrite("modified dataset/markers/"+ntpath.basename(name)[:-4]+".png", mark)
+	print(i, end=" ")
